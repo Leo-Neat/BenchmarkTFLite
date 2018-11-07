7e5f0cf635925ca28d360dc03280e5c17a33fd73
Leo Neat <lneat@ucsc.edu>
2018-11-06 19:43:48 -0800

    Camera preview up and running

--- app/src/main/java/com/leo/neat/benchmark_tf_lite/MainActivity.java
+++ app/src/main/java/com/leo/neat/benchmark_tf_lite/MainActivity.java
@@ -1,7 +1,15 @@
 package com.leo.neat.benchmark_tf_lite;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // XML linking
+        Button startBtn = findViewById(R.id.start_btn);
+        Button settingsBtn = findViewById(R.id.settings_btn);
+        Button resultsBtn = findViewById(R.id.results_btn);
+
+        // On button press
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), InferenceTestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button not implemented yet....",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        resultsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button not implemented yet....",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Request Camera permissions
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 }
